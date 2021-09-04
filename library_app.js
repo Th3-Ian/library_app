f2b165379a0c927b1fe65e09a9d5ffc3e7e7b2a4
Ian Hart <ianhart13@gmail.com>
2021-09-04 15:26:24 -0400

    initial commit - book func & addBook func created

---
+++ library_app.js
@@ -0,0 +1,27 @@
+let my_library = [];
+
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+	this.pages = pages
+	this.read = read
+  this.sayName = function() {
+    console.log(title, author, pages, read)
+  }
+}
+
+function addBookToLibrary(new_book) {
+	if (my_library.includes(new_book)) {
+		console.log("This book is already in the library");
+	}
+	else {
+		my_library.push(new_book);
+	}
+}
+
+const the_hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read')
+
+the_hobbit.sayName()
+addBookToLibrary(the_hobbit)
+
+console.log(my_library)
