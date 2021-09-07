2034e500e324958997c75d4a41ee8eb723c0b0a8
Ian Hart <ianhart13@gmail.com>
2021-09-06 21:00:34 -0400

    fixed addBookToLibrary to grab form values

--- library_app.js
+++ library_app.js
@@ -10,31 +10,34 @@ function Book(title, author, pages, read) {
 }
 
 Book.prototype.sayName = function() {
-	document.getElementById("title_output").innerHTML = this.title, this.author, this.pages, this.read;
-	document.getElementById("author_output").innerHTML = this.author, this.pages, this.read;
-	document.getElementById("pages_output").innerHTML = this.pages, this.read;
+	document.getElementById("title_output").innerHTML = this.title;
+	document.getElementById("author_output").innerHTML = this.author;
+	document.getElementById("pages_output").innerHTML = this.pages;
 	document.getElementById("read_output").innerHTML = this.read;
 }
 
-function addBookToLibrary(new_book) {
-	/*
-	var new_book = new Book(
-		document.getElementById("title"),
-		document.getElementById("author"),
-		document.getElementById("pages"),
-		document.getElementById("read")
-		);
-		*/
+function addBookToLibrary() {
+	var t = document.getElementById("title").value;
+	var a = document.getElementById("author").value;
+	var p = document.getElementById("page").value;
+	var r = document.getElementById("read").value;
+
+	var new_book = new Book( t, a, p, r);
 
 	if (my_library.includes(new_book)) {
 		console.log("This book is already in the library");
 	}
 	else {
 		my_library.push(new_book);
-		console.log(my_library)
+		var t = document.querySelector('table');
+		t.remove();
+		showLibrary();
+		console.log(my_library);
 	}
 }
 
+// TESTING VARIABLES
+/*
 const the_hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read')
 const two_towers = new Book('The Twin Towers', 'J.R.R. Tolkien', '348', 'read')
 
@@ -42,7 +45,9 @@ addBookToLibrary(the_hobbit)
 addBookToLibrary(two_towers)
 the_hobbit.sayName()
 two_towers.sayName()
+*/
 
+/* FIRST Show Function
 function showLibrary() {
 	output = "";
    for(var i = 0; i < my_library.length; i++) {
@@ -50,66 +55,68 @@ function showLibrary() {
    }
    document.getElementById("output").innerHTML = output;
 }
-
+*/
 // Table Constructor
+function showLibrary() {
+	var t = document.createElement('table');
+	t.classList.add('book-table', 'listing');
 
-var t = document.createElement('table');
-t.classList.add('book-table', 'listing');
+	t.appendChild(document.createElement('thead'));
+	t.querySelector('thead').appendChild(document.createElement('tr'));
 
-t.appendChild(document.createElement('thead'));
-t.querySelector('thead').appendChild(document.createElement('tr'));
+	for (var i = 0; i < lib_cols.length; i++) {
+		var heading = document.createElement('td');
+		heading.textContent = lib_cols[i];
+		t.querySelector('thead tr').appendChild(heading);
+	}
 
-for (var i = 0; i < lib_cols.length; i++) {
-	var heading = document.createElement('td');
-	heading.textContent = lib_cols[i];
-	t.querySelector('thead tr').appendChild(heading);
-}
-
-document.getElementById('container').appendChild(t);
+	document.getElementById('container').appendChild(t);
 
 
 // Create Table Rows
 
-for (var i = 0; i < my_library.length; i++) {
-	var s = my_library[i];
-	var r = document.createElement('tr');
+	for (var i = 0; i < my_library.length; i++) {
+		var s = my_library[i];
+		var r = document.createElement('tr');
 
-	r.dataset.personId = s.id;
-	r.id = s.title.toLowerCase() + "-row";
+		r.dataset.personId = s.id;
+		r.id = s.title + "-row";
 
-	var titleCell = document.createElement('td');
-	titleCell.textContent = s.title;
-	titleCell.classList.add('title');
-	titleCell.dataset.personId = s.id;
+		var titleCell = document.createElement('td');
+		titleCell.textContent = s.title;
+		titleCell.classList.add('title');
+		titleCell.dataset.personId = s.id;
 
-	var authorCell = document.createElement('td');
-	authorCell.textContent = s.author;
-	authorCell.classList.add('author');
-	authorCell.dataset.personId = s.id;
+		var authorCell = document.createElement('td');
+		authorCell.textContent = s.author;
+		authorCell.classList.add('author');
+		authorCell.dataset.personId = s.id;
 
-	var pageCell = document.createElement('td');
-	pageCell.textContent = s.pages;
-	pageCell.classList.add('pages');
-	pageCell.dataset.personId = s.id;
+		var pageCell = document.createElement('td');
+		pageCell.textContent = s.pages;
+		pageCell.classList.add('pages');
+		pageCell.dataset.personId = s.id;
 
-	var readCell = document.createElement('td');
-	readCell.textContent = s.read;
-	readCell.classList.add('read');
-	readCell.dataset.personId = s.id;
+		var readCell = document.createElement('td');
+		readCell.textContent = s.read;
+		readCell.classList.add('read');
+		readCell.dataset.personId = s.id;
 
-	var delCell = document.createElement('td');
-	delCell.textContent = "X";
-	delCell.classList.add('delete-cell');
+		var delCell = document.createElement('td');
+		delCell.textContent = "X";
+		delCell.classList.add('del-cell');
 
-	r.appendChild(titleCell);
-	r.appendChild(authorCell);
-	r.appendChild(pageCell);
-	r.appendChild(readCell);
-	r.appendChild(delCell);
+		r.appendChild(titleCell);
+		r.appendChild(authorCell);
+		r.appendChild(pageCell);
+		r.appendChild(readCell);
+		r.appendChild(delCell);
 
-	t.appendChild(r);
+		t.appendChild(r);
+	}
 }
 
+showLibrary();
 // Update Read Status
 
 const readSelector = document.querySelectorAll(".read");
@@ -121,6 +128,17 @@ for (let i = 0; i < readSelector.length; i++) {
 	});
 }
 
+// Delete Button
+
+const delBtn = document.querySelectorAll(".del-cell");
+
+for (let i = 0; i < delBtn.length; i++) {
+	delBtn[i].addEventListener("click", function(){
+		var row = document.querySelector('table').getElementsByTagName('tr');
+	row[i + 1].remove();
+
+	});
+}
 // New Book Button
 
 const toggleModal = () => {
@@ -133,5 +151,6 @@ const toggleModal = () => {
 
  document.querySelector('#open-form').addEventListener('submit', (event) => {
 	 event.preventDefault();
+	 addBookToLibrary();
 	 toggleModal();
  });
